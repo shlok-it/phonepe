62a41fed3e370b7c69e37fcc0c0c33e117edaeb7
nishad g <nishad g@DESKTOP-0EQP9NU>
2024-06-25 18:31:29 +0530

    first commit

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-import { PhonepePlugin } from './definitions';
-
-const Phonepe = registerPlugin<PhonepePlugin>('Phonepe', {
-    web: () => import('./web').then(m => new m.PhonepeWeb()),
-  });
-export * from './definitions';
-export { Phonepe };
